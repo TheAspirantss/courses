138ab164138efda589d3e39674b38ae033eeed7d
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2023-07-14 21:29:56 +0530

    first time uploaded all.

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    setTimeout(function () {
+      var loader = document.getElementById("loader");
+      loader.style.display = "none";
+    }, 2000);
+  });
+  
